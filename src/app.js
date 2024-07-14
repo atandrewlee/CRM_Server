46f1248f0e82639e172c174320bd3cf3b063a4a1
Andrew Lee <atandrewlee@gmail.com>
2024-07-14 11:32:59 -0400

    basic app.js (cron, listen)

--- src/app.js
+++ src/app.js
@@ -1,28 +1,13 @@
-// //Load HTTP module
-// import http from "http";
-// const hostname = "127.0.0.1";
-// const port = 3000;
-
-// //Create HTTP server and listen on port 3000 for requests
-// const server = http.createServer((req, res) => {
-//   //Set the response HTTP header with HTTP status and Content type
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World\n");
-// });
-
-// //listen for request on port 3000, and as a callback function have the port listened on logged
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
+import cron from "node-cron";
 import express from "express";
+
+cron.schedule('* * * * * *', () => {
+    console.log('run task every second');
+})
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
