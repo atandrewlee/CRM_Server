b6b3fdb5cdd3f0dce3c7112c3da0e2b4a950db49
Andrew Lee <atandrewlee@gmail.com>
2024-07-15 15:16:44 -0400

    Dockerfile

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import cron from "node-cron";
 import express from "express";
 
-cron.schedule('* * * * * *', () => {
-    console.log('run task every second');
-})
+// cron.schedule('* * * * * *', () => {
+//     console.log('run task every second');
+// })
 
 const app = express();
 const port = 3000;
+
+// Webhook Print
+app.get('/', (req, res) => {
+    console.log(req);
+    console.log(res);
+    res.send("Hello, World!");
+})
+
 
 
 app.listen(port, () => {
