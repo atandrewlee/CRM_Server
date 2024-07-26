61e54452d32d6aad23a65b7f1824c0f6ee99ac05
Andrew Lee <atandrewlee@gmail.com>
2024-07-26 10:33:06 -0400

    moved to dropbox sdk & oauth

--- README.md
+++ README.md
@@ -17,8 +17,7 @@ This is a web server & job runner for my personal CRM.
 - .env file
 
 ### What environment variables do you need?
-- DROPBOX_API= Dropbox API Key [docs](https://www.dropbox.com/developers/documentation/http/documentation)
-  - Refer to - FILE_PATH= File Path to the CRM directory
+- FILE_PATH= File Path to the CRM directory
   - POSIX Format
   - Root: Dropbox Root
   - Example: `/directory/crm_files
--- src/listener/createDropboxFile.js
+++ src/listener/createDropboxFile.js
@@ -1,25 +1,10 @@
-import axios from "axios";
-
-const DROPBOX_URL =  "https://content.dropboxapi.com/2/files/upload";
+// import axios from "axios";
+import { dbx } from "../app.js";
 
 export function dropbox_create_file(title) {
-    const options = {
-        method: 'POST',
-        url: DROPBOX_URL,
-        headers: {
-            'Authorization': `Bearer ${process.env.DROPBOX_API}`,
-            'Dropbox-API-Arg': JSON.stringify({
-                mode: "add",
-                path: `/ionia/crm/${title}`
-            }), 
-            'Content-Type': "application/octet-stream"
-        }
-    }
-    axios(options).then((response) => {
-        console.log(response.data);
-        return response.data;
-    }).catch((error) => {
-        console.log(error);
-        return error;
-    })
+    dbx.filesUpload({ path: process.env.CRM_FILE_PATH + title + ".md"}).then((response) => {
+        console.log(response);
+    }).catch((uploadErr) => {
+        console.log(uploadErr);
+    }) 
 }
