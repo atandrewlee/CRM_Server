898e86d55795852856b10bea6a9ac8fb6b3392ca
Andrew Lee <atandrewlee@gmail.com>
2024-08-08 17:09:29 -0400

    added yaml topmatter when file is created

--- src/listener/createFileFromNewPerson.js
+++ src/listener/createFileFromNewPerson.js
@@ -1,4 +1,5 @@
 import { dropboxUploadFile } from "../util/dropbox.js"
+import { STARTING_CRM_PAGE_TEMPLATE } from "../util/constants.js"
 
 export function createNewUserUpdate(req, res) {
     const before_row = req.body.data.previous_rows[0]
@@ -6,13 +7,13 @@ export function createNewUserUpdate(req, res) {
 
     // New Row is Created w/ a Name
     if (before_row.Name === null && after_row.Name !== null && typeof(after_row.Name) === 'string') {
-        res.send(dropboxUploadFile(process.env.CRM_FILE_PATH + after_row.Name + ".md", ""))
+        res.send(dropboxUploadFile(process.env.CRM_FILE_PATH + after_row.Name + ".md", STARTING_CRM_PAGE_TEMPLATE))
     }
 }
 
 export function createNewUserInsert(req, res) {
     const row = req.body.data.rows[0]
     if (row.Name !== null) {
-        res.send(dropboxUploadFile(process.env.CRM_FILE_PATH + row.Name + ".md"), "");
+        res.send(dropboxUploadFile(process.env.CRM_FILE_PATH + row.Name + ".md"), STARTING_CRM_PAGE_TEMPLATE);
     }
 }
